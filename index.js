fb4acc608241e3757247d29e196f88fad3183486
sukhpreet <glorymopi@gmail.com>
2024-01-19 15:21:24 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+let dodgers = document.getElementById('dodger')
+
+function moveDodgerLeft() { 
+    let leftnumbers = dodgers.style.left.replace("px", "");
+    let left = parseInt(leftnumbers, 10)
+    if (left > 0){ 
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+function moveDodgerRight() { 
+    let leftnumbers = dodger.style.left.replace("px", '')
+    let left = parseInt(leftnumbers, 10)
+    if (left < 360) { 
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+document.addEventListener("keydown",  function(e) { 
+    if (e.key === "ArrowLeft") { 
+        moveDodgerLeft()
+    }
+})
+
+document.addEventListener("keydown", function(e) { 
+    if (e.key === "ArrowRight") { 
+        moveDodgerRight
+    }
+})
